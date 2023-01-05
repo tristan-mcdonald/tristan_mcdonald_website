e02929b4539140c75a5ccbcbf5f5864f38d0dd4b
trubblebruin <letswork@tristanmcdonald.co.uk>
2023-01-05 14:22:13 +0000

    Remove build files, update text and background colour

--- src/assets/js/modules/keyboard-focus.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    init: () => {
-        /* global ally */
-        // detect focus source using a11y.js
-        const focusSource = ally.style.focusSource(); // eslint-disable-line no-unused-vars
-    },
-};
